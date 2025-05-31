1a91e63ea856fddee0f5e0db4a74262e10b8e974
firedevelop <firedevelop.com@gmail.com>
2025-05-31 11:18:11 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import Guitar from "./components/Guitar"
 import Header from "./components/Header"
 
 function App() {
     const [auth, setAuth] = useState([])
+    const [total, setTotal] = useState(0)
+    const [cart, setCart] = useState([])
 
 
   return (
@@ -12,7 +14,7 @@ function App() {
     
 
     <main className="container-xl mt-5">
-        <h2 className="text-center">Nuestra Colección3</h2>
+        <h2 className="text-center">Nuestra Colección8</h2>
         <div className="row mt-5">
           <Guitar></Guitar>
           <Guitar></Guitar>
